4f48b23611f863fff2156b808426f4ba78e12f82
kong6670 <30824358+kong6670@users.noreply.github.com>
2021-04-29 14:35:40 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if ( sizeof($request_array['events']) > 0 ) {
         $reply_token = $event['replyToken'];
 
         // $text = $event['message']['text'];
-        $text = array(
+        $messages = array(
 		  "type" => "sticker",
 		  "packageId" => "789",
 		  "stickerId" => "10855"
@@ -27,7 +27,7 @@ if ( sizeof($request_array['events']) > 0 ) {
         $data = [
             'replyToken' => $reply_token,
             // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-            'messages' => [['type' => 'text', 'text' => $text ]]
+            'messages' => $messages
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
