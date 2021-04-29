f4b748e5098814e1f4c7de506c52fe596901cd9b
kong6670 <30824358+kong6670@users.noreply.github.com>
2021-04-29 14:37:47 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ if ( sizeof($request_array['events']) > 0 ) {
         $data = [
             'replyToken' => $reply_token,
             // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
-            'messages' => $messages
+            'messages' => json_encode($messages)
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
